dd0b7548f25d90ac2bea65eb96da42e5af2aa584
lwy_notebook <liuwy@huayitech.com>
2013-09-26 18:07:04 +0800

    add admin

--- hycrm/views.py
+++ hycrm/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 import datetime
-def current_datetime(request):
+def login_view(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
