7a05a1dbc2aacf36355341ba1dedb027765a978a
lwy_notebook <liuwy@huayitech.com>
2013-09-30 18:08:24 +0800

    初步编写数据模型类

--- hycrm/models.py
+++ hycrm/models.py
@@ -1,3 +1,69 @@
+# coding=utf-8
 from django.db import models
-
+# 客户	customer
+# 联系人	contact
+# 业务机会	sale_opportunity
+# 立项申请表	project_apply
+# 周计划	Weekly_plan
+# 出差申请表	 travel_request
+# 出差汇报表	travel_report
+# 业务费用申请报销表	budget_apply
+# 商务支持申请	bussiness_apply
+# 统计报表	sale_report
 # Create your models here.
+# 在admin界面里配置的用户是对功能进行增删改的
+# 自己写的是在界面上对增删改进行控制的
+# http://djangobook.py3k.cn/2.0/
+# 客户名称、行业、电话、地点、费用累计
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    kind = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=30)
+    budget = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
+# 姓名、职务、性别、对应客户、部门、电话、手机、电子邮件、费用次数、费用累计
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    duty = models.CharField(max_length=30)
+    customer = models.ForeignKey(Customer)
+    department = models.CharField(max_length=30)
+    telephone = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    budget_count = models.CharField(max_length=30)
+    budget_sum = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.name
+# 基本信息（业务机会名称、对应客户、对应联系人、竞争对手分析、阶段、是否立项审批、推荐产品、客户决策链）；
+# 项目信息（预先销售额、预计毛利、占年度目标比例、预计招标日期、签约时间、厂家支持率、是否报备、目前费用总和、目前面临的问题、下一步工作计划、资源需求、已处于本阶段几周、何时进入下一阶段、赢率）；
+# 备注
+
+class Sale_opportunity(models.Model):
+    name = models.CharField(max_length=30)
+    customer = models.ForeignKey(Customer)
+    contact = models.ForeignKey(Contact)
+    competitors_info = models.CharField(max_length=60)
+    phase = models.CharField(max_length=30)
+    project_apply_approved  = models.CharField(max_length=30)
+    recommend_products = models.CharField(max_length=30)
+    customer_decision = models.CharField(max_length=30)
+    projected_sales = models.CharField(max_length=30)
+    projected_gross_profit = models.CharField(max_length=30)
+    annual_goal_percentage = models.CharField(max_length=30)
+    expected_tender_date = models.CharField(max_length=30)
+    sign_Time = models.CharField(max_length=30)
+    manufacturers_support_rate = models.CharField(max_length=30)
+    is_filing = models.CharField(max_length=30)
+    current_budget_sum = models.CharField(max_length=30)
+    current_problem = models.CharField(max_length=30)
+    following_plan = models.CharField(max_length=30)
+    resource_Requirements = models.CharField(max_length=30)
+    current_week = models.CharField(max_length=30)
+    next_phase_time = models.CharField(max_length=30)
+    success_chance = models.CharField(max_length=30)
+    note = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.name
+
--- hycrm/views.py
+++ hycrm/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # Create your views here.
 from django.http.response import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate,login
@@ -10,9 +11,11 @@ def login_view(request):
     user = authenticate(username=request.POST['username'], password=request.POST['password'])
     if user is not None:
         login(request, user)
-        return render_to_response("main.html")
+
+    if user.username == "lwy":
+        return render_to_response('main.html',{'username':'lwy'})
     else:
-        return weidenglu(request)
+        return render_to_response('main.html',{'username':'admin'})
 
 def denglu(request):
     return  HttpResponse("denglule")
